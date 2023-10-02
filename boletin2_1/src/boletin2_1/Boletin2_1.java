a3220df94e5928e8c39d49a93260cde3d97cbfde
Adrian <aabeijoncarbajo@danielcastelao.org>
2023-10-02 09:18:21 +0200

    commit 2_1 java

---
+++ boletin2_1/src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package boletin2_1;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Boletin2_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float base = 4, altura = 3, area;
+        area = (base * altura)/2;
+        System.out.println("Area do triangulo = "+area);
+    }
+    
+}
