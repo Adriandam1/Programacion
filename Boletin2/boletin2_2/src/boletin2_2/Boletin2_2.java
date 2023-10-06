7bf4e784794ea92805fb995e693a6810ec86201c
Adriandam1 <72071798+Adriandam1@users.noreply.github.com>
2023-10-06 08:34:16 +0200

    Add files via upload
    boletin2

---
+++ Boletin2/boletin2_2/src/boletin2_2/Boletin2_2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package boletin2_2;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Boletin2_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                float lado = 3, area;
+        area = lado*lado;
+        System.out.println("Area do cuadrado= "+area);
+    }
+    
+}
