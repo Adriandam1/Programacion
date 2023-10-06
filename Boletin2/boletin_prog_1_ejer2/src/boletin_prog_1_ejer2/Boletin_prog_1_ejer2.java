2c0e647ef44ba8b709c4b952224af5b133ef713e
Adriandam1 <72071798+Adriandam1@users.noreply.github.com>
2023-10-06 08:37:15 +0200

    Add files via upload

---
+++ Boletin2/boletin_prog_1_ejer2/src/boletin_prog_1_ejer2/Boletin_prog_1_ejer2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package boletin_prog_1_ejer2;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin_prog_1_ejer2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 3;
+        i++;
+        System.out.println(i); // 4
+        ++i;
+        System.out.println(i); // 5
+        System.out.println(++i); // 6
+        System.out.println(i++); // 6	
+        System.out.println(i); // 7
+    }
+    
+}
