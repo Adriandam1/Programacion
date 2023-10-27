ed80c4c293a9e5040b3b1716ddbb3668e7543b61
Adriandam1 <72071798+Adriandam1@users.noreply.github.com>
2023-10-27 13:40:52 +0200

    corregido scanner

---
+++ Boletin9/boletin9_1/src/boletin9_1/Boletin9_1.java
@@ -0,0 +1,19 @@
+/*
+Calcular a cantidade de numeros positivos, negativos e ceros que hay nun grupo de 10 numeros enteiros.
+ */
+package boletin9_1;
+
+
+/**
+ *
+ * @author Adrian
+ */
+public class Boletin9_1 {
+
+    public static void main(String[] args) {
+            NumerosEnteiros obx = new NumerosEnteiros();
+            obx.dezNumeros();        
+        
+    }
+    
+}
