7062a20035eed33bdf1c775ff50484a38f69db4f
Adriandam1 <72071798+Adriandam1@users.noreply.github.com>
2023-12-01 12:59:24 +0100

    subo el segundo ejercicio

---
+++ Boletins18/Boletins18_2/src/boletins18_2/Boletins18_2.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package boletins18_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Boletins18_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String repetir;
+        Metodos obx = new Metodos();
+        
+        do{
+        obx.getprezo();
+        
+        System.out.println("Se desexa enviar mais paquetes escriba 'si'");
+        repetir = sc.nextLine();
+        }while ("si".equals(repetir));
+        
+    }
+    
+}
---
+++ Boletins18/Boletins18_2/src/boletins18_2/Metodos.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package boletins18_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Metodos {
+    int zona;
+    float custo;
+    
+    Scanner sc = new Scanner(System.in);
+
+    public Metodos() {
+    }
+
+    public Metodos(int zona, float custo) {
+        this.zona = zona;
+        this.custo = custo;
+    }
+    
+    public void getprezo(){
+        float peso;
+        // En este do saco el menu para elegir el destino
+        do{
+            System.out.println("Indique o destino:\n1- America do Norte\n2- America Central\n3- America do Sur\n4- Europa\n5- Asia ");
+            zona = sc.nextInt();
+            
+           switch ( zona){
+            case 1:
+                custo = 24;
+                break;
+            case 2:
+                custo = 20;
+                break;
+            case 3:
+                custo = 21;
+                break;
+            case 4:
+                custo = 10;
+                break;
+            case 5:
+                custo = 18;
+                break;
+        }        
+        }while (zona < 1 || zona >5);
+        // En este do indico el peso del paquete
+        do{
+            System.out.println("Indique o peso do paquete en KG: ");
+            peso = sc.nextFloat();
+            if (peso >5)
+                System.out.println("ERROR o peso maximo do paquete e de 5 KG ");
+            if (peso<=0)
+                System.out.println("O paquete debe tener peso");
+            
+        }while (peso >5 || peso <=0);
+        // outprint con el precio del paquete
+        System.out.println("O prezo do envio e: "+(custo * peso)+" €");
+        
+        
+    }
+    
+}
