ff2313cc3908888b9a74b6a4623890147aa0505d
Adriandam1 <72071798+Adriandam1@users.noreply.github.com>
2023-12-13 10:53:50 +0100

    Add files via upload

---
+++ Boletin9/boletin6_3/src/boletin6_3/Boletin6_3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package boletin6_3;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Boletin6_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Circulo obx = new Circulo();
+        obx.calcularArea();
+        obx.calcularLonxitude();
+    }
+    
+}
