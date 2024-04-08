97824fa5399072cba03d09788dcece7d2b0e2ad3
Adriandam1 <72071798+Adriandam1@users.noreply.github.com>
2024-04-08 09:22:57 +0200

    comentario enunciado ejer2

--- TareaGestionEventos2/Tarea_gestion_eventos2-2/src/tarea_gestion_eventos2/Tarea_gestion_eventos22.java
+++ TareaGestionEventos2/Tarea_gestion_eventos2-2/src/tarea_gestion_eventos2/Tarea_gestion_eventos22.java
@@ -1,6 +1,22 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+Enunciado 2: Validador de Números
+Objetivo: Crear una aplicación simple de consola que solicite al usuario
+ingresar números hasta que introduzca un número específico (por ejemplo, el
+número 0). Utilizar una interfaz con un método que valide si el número
+ingresado cumple con una condición particular (por ejemplo, si es par o
+impar) y mostrar un mensaje en consola. Implementar la lógica de validación
+dentro de una clase anónima.
+Requisitos:
+Definir una interfaz Validador con un método que acepte un número y
+devuelva boolean.
+Solicitar al usuario que ingrese números a través de la consola.
+Utilizar Scanner para leer la entrada del usuario.
+Implementar una clase anónima que realice la validación de números (par o
+impar) utilizando la interfaz Validador.
+Incorporar un bucle para repetir la solicitud de números hasta que el usuario
+introduzca el número 0.
+Manejar adecuadamente las excepciones que puedan ocurrir al leer la
+entrada del usuario.
  */
 package tarea_gestion_eventos2;
 
